60d6c0d3df8a2dd5dabbfc9b2b6acf8875ae18af
jangins101 <jangins101@gmail.com>
2015-11-23 15:41:18 -0500

    Update test.js

--- TamperMonkey/test.js
+++ TamperMonkey/test.js
@@ -5,6 +5,8 @@
 // @match https://*/tmui/Control/*
 // @match https://*/sam/admin/reports/*
 // @run-at          document-end
+// @updateURL https://github.com/jangins101/F5/edit/master/TamperMonkey/test.js
+// @downloadURL https://github.com/jangins101/F5/edit/master/TamperMonkey/test.js
 // @grant none
 // @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==

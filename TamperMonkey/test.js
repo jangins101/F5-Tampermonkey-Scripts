6d7febd24e8dacab23fbbba4daf30edb89daeaed
jangins101 <jangins101@gmail.com>
2015-11-23 15:40:38 -0500

    Create test.js

---
+++ TamperMonkey/test.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name BigiIP UI Tweaks
+// @version 0.5
+// @description Tweaks the F5 UI
+// @match https://*/tmui/Control/*
+// @match https://*/sam/admin/reports/*
+// @run-at          document-end
+// @grant none
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+ 
+ 
+ alert("testing")

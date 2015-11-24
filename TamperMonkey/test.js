805e71f2b34cbfa002aee069a35c488b43f27121
jangins101 <jangins101@gmail.com>
2015-11-24 08:30:02 -0500

    Update test.js

--- TamperMonkey/test.js
+++ TamperMonkey/test.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name Testing again
-// @version 0.6
+// @version 0.61
 // @description Tweaks the F5 UI
 // @match https://*/tmui/Control/*
 // @match https://*/sam/admin/reports/*
 // @run-at          document-end
+// @updateURL https://raw.githubusercontent.com/jangins101/F5/master/TamperMonkey/test.js
 // @downloadURL https://raw.githubusercontent.com/jangins101/F5/master/TamperMonkey/test.js
 // @grant none
 // @require http://code.jquery.com/jquery-latest.js

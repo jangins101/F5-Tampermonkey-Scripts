e401f22df47de57bc8923cc0de682173db5deb2b
jangins101 <jangins101@gmail.com>
2015-12-03 12:23:43 -0500

    Update F5 BIG-IP UI Enhancements.js

--- TamperMonkey/F5 BIG-IP UI Enhancements.js
+++ TamperMonkey/F5 BIG-IP UI Enhancements.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name F5 BIG-IP UI Enhancements
-// @version 1.0
+// @version 1.1
 // @homepage https://github.com/jangins101/F5/blob/master/TamperMonkey/F5%20BIG-IP%20UI%20Enhancements.js
 // @description Adds a lot of useful features to the GUI in order to make access to different configuration items quicker 
 // @match https://*/tmui/Control/*

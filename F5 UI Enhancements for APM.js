7e0c0b9ae6a1f02feeaa3c090771a4218ebbb429
Michael Jenkins <jangins101@gmail.com>
2016-07-18 13:41:32 -0400

    revert back to standalone file

--- F5 UI Enhancements for APM.js
+++ F5 UI Enhancements for APM.js
@@ -44,7 +44,62 @@
 */
 
 
-dlog("F5 UI Enhancements for APM loading", 1);
+// Turns on logging for the script (useful for debugging issues)
+//  0 - Off
+//  1 - Notice
+//  2 - Informational
+//  3 - Debug
+var DebugLevel = 3;
+
+
+//Make sure that the tampermonkey jQuery does not tamper with F5's scripts
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+
+// ***********************************
+// ***** SECTION: Helper Functions ***
+// ***********************************
+
+// If the IsDebug setting not empty/undefined/false, then we'll log messages to the console
+function dlog(o, minLevel) {
+    if (DebugLevel && (!minLevel || (minLevel >= DebugLevel) )) { console.log(o); } 
+}
+
+// Most functionality is specific to a certain page, so this function is used to check the current page against a specific URL 
+function checkLocation(str) {
+    return (window.location.href.indexOf(str) >= 0);
+}
+
+// This will parse the specified url parameter value form the current page URL if it exists
+function getParameterByName(name) {
+    // REF: http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+// This will grab the specified cookie if it exists
+function getCookie(cname) {
+    // REF: http://www.w3schools.com/js/js_cookies.asp
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) === 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+// This will redirect the user the specified url (in a new window if specified)
+function redirect(url, newWindow) {
+    if (newWindow) {
+        window.open(url);
+    } else {
+        window.location = url;
+    }
+}
 
 // ***********************************
 // ***** SECTION: Custom CSS *********
@@ -72,6 +127,8 @@ $("<style type='text/css'> \
   </style>").appendTo("head");
 
 
+
+
 // When debugging this script, it's nice to know what the current URL is that we're working with
 dlog("Location: " + window.location.href, 3);
 
@@ -82,7 +139,7 @@ dlog("Location: " + window.location.href, 3);
 
 // Access Profile List
 if (checkLocation("/tmui/Control/jspmap/tmui/accessctrl/profiles/list.jsp") || checkLocation("/tmui/Control/form?__handler=/tmui/accessctrl/profiles/list")) {
-    dlog("Access Policy | List ");
+    console.log("Access Policy | List ");
 
     // Convert the Status Flag icon to a link that will apply only that Access Policy
     var rows = $("table#list_table tbody tr");

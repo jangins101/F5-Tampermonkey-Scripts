736af2cc0e1eb643ebff914d3545b28a82df3a9e
Michael Jenkins <jangins101@gmail.com>
2016-07-20 12:12:20 -0400

    version inc

--- F5 Devcentral Enhancements.js
+++ F5 Devcentral Enhancements.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name F5 Devcentral Enhancements
-// @version 1.0
+// @version 1.02
 // @homepage https://github.com/jangins101/F5/blob/master/F5%20Devcentral%20Enhancements.js
 // @description Adds a lot of useful features to the GUI in order to make access to different configuration items quicker
 // @updateURL https://github.com/jangins101/F5/raw/master/F5%20Devcentral%20Enhancements.js
